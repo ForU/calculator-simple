e8bd6a43942b02dcdbd6093ce6d8771b117cf2b3
lk <515563130@qq.com>
2013-12-26 04:03:15 +0800

    divides files

--- calculator_core.cc
+++ calculator_core.cc
@@ -1,4 +1,4 @@
-#include "calculator.hh"
+#include "calculator_core.hh"
 #include "log.hh"
 
 #include <string.h>
@@ -257,20 +257,3 @@ bool ArithmeticExpression::parse()
     return true;
 }
 
-int main(int argc, char *argv[])
-{
-    if ( argc == 1 ) {
-        PR_TRACE("Usage: %s \"infix_expression\" ...", argv[0]);
-        return 0;
-    }
-
-    const char* sep = "________________________________________________________________";
-    for ( int i = 1; i < argc;  ++i) {
-        PR_TRACE("%s\"%s\"", sep, argv[i]);
-        ArithmeticExpression ari_exp(argv[i]);
-        ari_exp.parse();
-        PR_TRACE("rpn = \"%s\"", ari_exp.getRpnExpression().c_str());
-    }
-
-    return 0;
-}
